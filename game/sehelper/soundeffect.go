959dfc4871bbc0b1290c3c71587139afdd5f79be
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 04:38:53 +0900

    Add doc to soundeffect

--- game/sehelper/soundeffect.go
+++ game/sehelper/soundeffect.go
@@ -6,24 +6,35 @@ import (
 	"github.com/veandco/go-sdl2/mix"
 )
 
+// SEType is kind of sound effect
 type SEType string
 
 const (
-	SuccessSE        SEType = "ses/success.wav"
+	// SuccessSE rings when succeed
+	SuccessSE SEType = "ses/success.wav"
+	// SpecialSuccessSE rings epically when succeed
 	SpecialSuccessSE SEType = "ses/special.wav"
-	FailedSE         SEType = "ses/failed.wav"
-	UnneccesarySE    SEType = "ses/unneccesary.wav"
-	GameoverSE       SEType = "ses/gameover.wav"
-	AcSE             SEType = "ses/ac.wav"
-	WaSE             SEType = "ses/wa.wav"
-	FastSE           SEType = "ses/fast.wav"
-	TleSE            SEType = "ses/tle.wav"
+	// FailedSE rings when failed
+	FailedSE SEType = "ses/failed.wav"
+	// UnneccesarySE rings when player did unneccesary thing
+	UnneccesarySE SEType = "ses/unneccesary.wav"
+	// GameoverSE rings when game was over
+	GameoverSE SEType = "ses/gameover.wav"
+	// AcSE rings when accepted
+	AcSE SEType = "ses/ac.wav"
+	// WaSE rings when wrong-answered
+	WaSE SEType = "ses/wa.wav"
+	// FastSE rings when player was faster
+	FastSE SEType = "ses/fast.wav"
+	// TleSE rings when player failed to type all
+	TleSE SEType = "ses/tle.wav"
 )
 
 var (
 	seCache = map[SEType]*mix.Chunk{}
 )
 
+// Play plays sound effect
 func Play(seType SEType) {
 	logger := Logger.NewLogger("Play")
 	SE, Exists := seCache[seType]
