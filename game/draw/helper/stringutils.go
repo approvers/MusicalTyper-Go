5727e1a72100fecce43a2575aef912f67d6c4caa
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 04:21:10 +0900

    Add docs to stringutils

--- game/draw/helper/stringutils.go
+++ game/draw/helper/stringutils.go
@@ -2,10 +2,12 @@ package helper
 
 import "unicode/utf8"
 
+// Substring slices string from a to b
 func Substring(s string, a, b int) string {
 	return string([]rune(s)[a:b])
 }
 
+// Length calculates length of string
 func Length(s string) int {
 	return utf8.RuneCountInString(s)
 }
