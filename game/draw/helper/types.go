012e42c2728596124ec3fd7d41133e322d7ee654
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 04:21:27 +0900

    Add doc to types

--- game/draw/helper/types.go
+++ game/draw/helper/types.go
@@ -1,10 +1,14 @@
 package helper
 
+// AlignmentType means kind of alignment
 type AlignmentType uint8
 
 const (
+	// RightAlign means to align to right
 	RightAlign AlignmentType = iota
+	// LeftAlign means to align to left
 	LeftAlign
+	// Center means to align to center
 	Center
 )
 
