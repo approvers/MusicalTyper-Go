3a9f4dc57f7cff2300f9d70340f2624ea3833590
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 07:18:22 +0900

    Use guards

--- game/draw/helper/math.go
+++ game/draw/helper/math.go
@@ -4,16 +4,14 @@ package helper
 func Max(a, b uint8) uint8 {
 	if a > b {
 		return a
-	} else {
-		return b
 	}
+	return b
 }
 
 // MinUInt8 returns min of a and b, then cast it uint8
 func MinUInt8(a, b int) uint8 {
 	if a < b {
 		return uint8(a)
-	} else {
-		return uint8(b)
 	}
+	return uint8(b)
 }
