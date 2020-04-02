554f8b39aea59825f1627ef137db1b72cf19749d
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 04:21:17 +0900

    Add doc to text

--- game/draw/helper/text.go
+++ game/draw/helper/text.go
@@ -10,7 +10,10 @@ import (
 	"github.com/veandco/go-sdl2/ttf"
 )
 
+// FontSize means size of font
 type FontSize uint8
+
+// TextureCache has texture with width and height
 type TextureCache struct {
 	Width, Height int32
 	Texture       *sdl.Texture
@@ -22,12 +25,18 @@ var (
 )
 
 const (
-	BigFont      FontSize = 72
-	JapaneseFont          = 48
-	AlphabetFont          = 32
-	FullFont              = 24
-	RankFont              = 20
-	SystemFont            = 16
+	// BigFont is bigger font size
+	BigFont FontSize = 72
+	// JapaneseFont is font size for japanese
+	JapaneseFont = 48
+	// AlphabetFont is font size for ASCII
+	AlphabetFont = 32
+	// FullFont is font size for full width
+	FullFont = 24
+	// RankFont is font size for rank of player
+	RankFont = 20
+	// SystemFont is default font size
+	SystemFont = 16
 )
 
 func getTextureCacheKey(Text string, Color *sdl.Color) string {
@@ -73,6 +82,7 @@ func makeTexture(Renderer *sdl.Renderer, Size FontSize, Text string, Color *sdl.
 	return Texture
 }
 
+// DrawText renders text
 func DrawText(Renderer *sdl.Renderer, x, y int, alignment AlignmentType, Size FontSize, Text string, Color *sdl.Color) (int, int) {
 	logger := Logger.NewLogger("DrawText")
 	if Text == "" {
@@ -111,6 +121,7 @@ func DrawText(Renderer *sdl.Renderer, x, y int, alignment AlignmentType, Size Fo
 	return int(Texture.Width), int(Texture.Height)
 }
 
+// DrawThickLine renders thick line
 //fixme: 計算ガバガバなので斜めの線とか引くと多分バグる
 func DrawThickLine(Renderer *sdl.Renderer, x, y, dstx, dsty int, Color *sdl.Color, Thickness int) {
 	Renderer.SetDrawColor(Color.R, Color.G, Color.B, Color.A)
@@ -118,20 +129,24 @@ func DrawThickLine(Renderer *sdl.Renderer, x, y, dstx, dsty int, Color *sdl.Colo
 	Renderer.DrawRect(&sdl.Rect{X, Y, DistX - X, DistY - Y})
 }
 
+// DrawLine render line
 func DrawLine(Renderer *sdl.Renderer, x, y, dstx, dsty int, Color *sdl.Color) {
 	DrawThickLine(Renderer, x, y, dstx, dsty, Color, 1)
 }
 
+// GetTextSize calculates dimension of text by actual rendering
 func GetTextSize(Renderer *sdl.Renderer, Size FontSize, Text string, Color *sdl.Color) (int, int) {
 	Texture := makeTexture(Renderer, Size, Text, Color)
 	return int(Texture.Width), int(Texture.Height)
 }
 
+// DrawFillRect renders filled rect
 func DrawFillRect(Renderer *sdl.Renderer, Color *sdl.Color, x, y, width, height int) {
 	Renderer.SetDrawColor(Color.R, Color.G, Color.B, Color.A)
 	Renderer.FillRect(&sdl.Rect{int32(x), int32(y), int32(width), int32(height)})
 }
 
+// DrawLineRect render rect by lines
 func DrawLineRect(Renderer *sdl.Renderer, Color *sdl.Color, x, y, width, height, thickness int) {
 	Renderer.SetDrawColor(Color.R, Color.G, Color.B, Color.A)
 	X, Y, Width, Height, Thickness := int32(x), int32(y), int32(width), int32(height), int32(thickness)
@@ -144,6 +159,7 @@ func DrawLineRect(Renderer *sdl.Renderer, Color *sdl.Color, x, y, width, height,
 	Renderer.DrawRects(Rects)
 }
 
+// Quit destroys textures
 func Quit() {
 	for _, v := range textureCache {
 		for _, t := range v {
