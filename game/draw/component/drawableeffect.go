5e5479a293a43df009e2eb320ac06ca8bc51a09d
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 03:43:29 +0900

    Add doc to drawableeffect

--- game/draw/component/drawableeffect.go
+++ game/draw/component/drawableeffect.go
@@ -4,10 +4,12 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+// DrawableEffect is renderer with EffectDrawContext
 type DrawableEffect interface {
 	Draw(*EffectDrawContext)
 }
 
+// EffectDrawContext is required state to present effect
 type EffectDrawContext struct {
 	Renderer   *sdl.Renderer
 	FrameCount int
