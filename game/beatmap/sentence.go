22ea53a90fff00c5b4c7cf9f5ed15aba1feac6e6
MikuroXina <ryosukadnak@gmail.com>
2020-04-03 01:17:19 +0900

    Add doc to sentence

--- game/beatmap/sentence.go
+++ game/beatmap/sentence.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// Sentence has state for typing roman string
 type Sentence struct {
 	OriginalSentence string
 	HiraganaSentence string
@@ -19,6 +20,7 @@ type Sentence struct {
 	IsFinished bool
 }
 
+// Character has japanese character, its roman styles, and typed index
 type Character struct {
 	//例: ち
 	Character string
@@ -30,6 +32,7 @@ type Character struct {
 	TypingIndex int
 }
 
+// NewSentence makes Sentence from japanese and hiragana
 func NewSentence(OriginalSentence, HiraganaSentence string) *Sentence {
 	Result := new(Sentence)
 	Result.HiraganaSentence = HiraganaSentence
@@ -39,14 +42,17 @@ func NewSentence(OriginalSentence, HiraganaSentence string) *Sentence {
 	return Result
 }
 
+// GetTypedText returns substring of hiragana before typed index
 func (s *Sentence) GetTypedText() string {
 	return DrawHelper.Substring(s.HiraganaSentence, 0, s.CurrentCharacterIndex)
 }
 
+// GetRemainingText returns substring of hiragana after typed index
 func (s *Sentence) GetRemainingText() string {
 	return DrawHelper.Substring(s.HiraganaSentence, s.CurrentCharacterIndex, DrawHelper.Length(s.HiraganaSentence))
 }
 
+// GetTypedRoma returns typed roman
 func (s *Sentence) GetTypedRoma() string {
 	if len(s.SolvedSentence) == 0 {
 		return ""
@@ -65,6 +71,7 @@ func (s *Sentence) GetTypedRoma() string {
 	return Result
 }
 
+// GetRemainingRoma returns standard style roman string to be inputted
 func (s *Sentence) GetRemainingRoma() string {
 	if len(s.SolvedSentence) == 0 {
 		return ""
@@ -77,6 +84,7 @@ func (s *Sentence) GetRemainingRoma() string {
 	return DrawHelper.Substring(Result, DrawHelper.Length(s.GetTypedRoma()), DrawHelper.Length(Result))
 }
 
+// GetRoma returns whole of roman string
 func (s *Sentence) GetRoma() string {
 	Result := ""
 	for i := 0; i < s.CurrentCharacterIndex; i++ {
@@ -85,6 +93,7 @@ func (s *Sentence) GetRoma() string {
 	return Result
 }
 
+// GetAccuracy return accuracy of user typed
 func (s *Sentence) GetAccuracy() float64 {
 	Misses, Types := 1, 1
 	if s.TypeCount > 0 {
@@ -97,6 +106,7 @@ func (s *Sentence) GetAccuracy() float64 {
 	return float64(Misses) / float64(Types)
 }
 
+// JudgeKeyInput decides and mutates Sentence state by inputted characters
 func (s *Sentence) JudgeKeyInput(input string) (ok, isThisSentenceEnded bool) {
 	CurrentChar := s.SolvedSentence[s.CurrentCharacterIndex]
 	fmt.Printf("Char:%d Typ: %d\n", s.CurrentCharacterIndex, CurrentChar.TypingIndex)
